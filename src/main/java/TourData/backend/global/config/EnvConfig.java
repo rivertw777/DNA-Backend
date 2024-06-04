c7a9a17d241ce51f580cba5aa6ffbc316a07adb5
rivertw777 <105557972+rivertw777@users.noreply.github.com>
2024-06-04 20:37:24 +0900

    feat: EnvConfig 작성

---
+++ src/main/java/TourData/backend/global/config/EnvConfig.java
@@ -0,0 +1,13 @@
+package TourData.backend.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+        @PropertySource("classpath:env.properties")
+})
+public class EnvConfig {
+
+}
