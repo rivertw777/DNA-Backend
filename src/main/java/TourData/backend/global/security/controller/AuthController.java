7d1ad0d53d2ca2db5ffbb27566cc92b02ff475f6
rivertw777 <105557972+rivertw777@users.noreply.github.com>
2024-06-29 19:22:56 +0900

    feat: 레디스 적용 (임베디드)

--- src/main/java/TourData/backend/global/security/controller/AuthController.java
+++ src/main/java/TourData/backend/global/security/controller/AuthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
-public class SecurityController {
+public class AuthController {
 
     // 인증 확인
     @GetMapping("/check")
