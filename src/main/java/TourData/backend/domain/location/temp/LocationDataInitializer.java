c29726ad4af592668471b2c2df562a13f40344be
rivertw777 <105557972+rivertw777@users.noreply.github.com>
2024-07-06 20:25:36 +0900

    refactor: LocationDataInitializer 수정

--- src/main/java/TourData/backend/domain/location/temp/LocationDataInitializer.java
+++ src/main/java/TourData/backend/domain/location/temp/LocationDataInitializer.java
@@ -17,21 +17,35 @@ public class LocationDataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        Location location1 = new Location( "Sokcho", "https://cdn.woman.chosun.com/news/photo/202204/97384_80024_1440.jpg");
+
+        Location location1 = new Location( "Sokcho", "https://images.unsplash.com/photo-1698767676786-03457d067360?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
         locationRepository.save(location1);
         locationLikeCountService.initCount(location1.getId());
 
-        Location location2 = new Location("Chuncheon", "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202104/15/9dc7194a-30cc-4005-aabc-372e362b29f9.jpg");
+        Location location2 = new Location("Chuncheon", "https://images.unsplash.com/photo-1622628036982-f82aa2fd4fc5?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
         locationRepository.save(location2);
         locationLikeCountService.initCount(location2.getId());
 
-        Location location3 = new Location("Yangyang", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZbXZWlokJE_MLQc1YqROH2doJihNzVIWxqg&s");
+        Location location3 = new Location("Yangyang", "https://images.unsplash.com/photo-1661926388505-334021844cec?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
         locationRepository.save(location3);
         locationLikeCountService.initCount(location3.getId());
 
-        Location location4 = new Location("Gangneung", "https://images.unsplash.com/photo-1621044332832-717d5d986ab7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+        Location location4 = new Location("Gangneung", "https://images.unsplash.com/photo-1684042229029-8a899193a8e4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
         locationRepository.save(location4);
         locationLikeCountService.initCount(location4.getId());
+
+        Location location5 = new Location("Taebaek", "https://images.unsplash.com/photo-1676705909297-9e172058d8d0?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+        locationRepository.save(location5);
+        locationLikeCountService.initCount(location5.getId());
+
+        Location location6 = new Location("Seoul", "https://images.unsplash.com/photo-1565149394348-c56457810899?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+        locationRepository.save(location6);
+        locationLikeCountService.initCount(location6.getId());
+
+        Location location7 = new Location("Anyang", "https://images.unsplash.com/photo-1644648123423-d3bd4b7ccc33?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+        locationRepository.save(location7);
+        locationLikeCountService.initCount(location7.getId());
+
     }
 
     @PreDestroy
