c5c4519332d542d50b8856dd295a86f95cca0261
rivertw777 <105557972+rivertw777@users.noreply.github.com>
2024-07-19 21:45:30 +0900

    feat: redisService 메서드 추가

--- src/main/java/TourData/backend/global/redis/service/RedisService.java
+++ src/main/java/TourData/backend/global/redis/service/RedisService.java
@@ -9,10 +9,26 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class RedisService {
 
-    private final RedisTemplate<String, String> redisTemplate;
+    private final RedisTemplate<String, Object> redisTemplate;
 
-    public void setValues(String key, String value, Long expiration) {
+    public void save(String key, Object value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public void saveWithExpiration(String key, Object value, long expiration) {
         redisTemplate.opsForValue().set(key, value, expiration, TimeUnit.MILLISECONDS);
+    }
+
+    public Object get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void delete(String key) {
+        redisTemplate.delete(key);
+    }
+
+    public boolean exists(String key) {
+        return redisTemplate.hasKey(key);
     }
 
 }
