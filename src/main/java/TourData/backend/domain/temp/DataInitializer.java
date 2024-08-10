dfd28ab6640add3062ca2a0012f0b261eb60e1c8
rivertw777 <105557972+rivertw777@users.noreply.github.com>
2024-08-10 14:37:49 +0900

    refactor: DataInitializer 수정

--- src/main/java/TourData/backend/domain/temp/DataInitializer.java
+++ src/main/java/TourData/backend/domain/temp/DataInitializer.java
@@ -25,47 +25,42 @@ public class DataInitializer implements CommandLineRunner {
     @Override
     public void run(String... args) {
 
-        Location location1 = Location.createLocation(LocationName.SOCKCHO, 38.2072, 128.5911,
+        Location location1 = Location.createLocation(LocationName.SOCKCHO, 38.2060, 128.5912,
                 "https://images.unsplash.com/photo-1698767676786-03457d067360?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
         locationRepository.save(location1);
         locationLikeCountService.initCount(location1.getId());
         participantCountService.initCount(location1.getId());
 
-        Location location2 = Location.createLocation( LocationName.CHUNCHEON, 37.8825, 127.7295,
+        Location location2 = Location.createLocation( LocationName.CHUNCHEON, 37.8814, 127.7259,
                 "https://images.unsplash.com/photo-1622628036982-f82aa2fd4fc5?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
         locationRepository.save(location2);
         locationLikeCountService.initCount(location2.getId());
         participantCountService.initCount(location2.getId());
 
-        Location location3 = Location.createLocation(LocationName.YANGYANG, 38.1304, 128.6615,
+        Location location3 = Location.createLocation(LocationName.YANGYANG, 38.0861, 128.6000,
                 "https://images.unsplash.com/photo-1661926388505-334021844cec?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
         locationRepository.save(location3);
         locationLikeCountService.initCount(location3.getId());
         participantCountService.initCount(location3.getId());
 
-        Location location4 = Location.createLocation(LocationName.GANGNEUNG, 37.7572, 128.8760,
+        Location location4 = Location.createLocation(LocationName.GANGNEUNG, 37.7515, 128.8760,
                 "https://images.unsplash.com/photo-1684042229029-8a899193a8e4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
         locationRepository.save(location4);
         locationLikeCountService.initCount(location4.getId());
         participantCountService.initCount(location4.getId());
 
-        Location location5 = Location.createLocation( LocationName.TAEBAEK, 37.1105, 128.9861,
+        Location location5 = Location.createLocation( LocationName.WONJU, 37.3495, 127.9219,
                 "https://images.unsplash.com/photo-1676705909297-9e172058d8d0?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
         locationRepository.save(location5);
         locationLikeCountService.initCount(location5.getId());
         participantCountService.initCount(location5.getId());
 
-        Location location6 = Location.createLocation( LocationName.YEONGWOL, 37.6778, 128.5573,
+        Location location6 = Location.createLocation( LocationName.SAMCHEOK, 37.4489, 129.1650,
                 "https://images.unsplash.com/photo-1565149394348-c56457810899?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
         locationRepository.save(location6);
         locationLikeCountService.initCount(location6.getId());
         participantCountService.initCount(location6.getId());
 
-        Location location7 = Location.createLocation(LocationName.DONGHAE, 37.5202, 129.1008,
-                "https://images.unsplash.com/photo-1644648123423-d3bd4b7ccc33?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
-        locationRepository.save(location7);
-        locationLikeCountService.initCount(location7.getId());
-        participantCountService.initCount(location7.getId());
 
         Facility facility1 = Facility.createFacility("맥도날드", FacilityType.RESTAURANT, LocationName.SOCKCHO,"강원도 속초시",37.3972, 126.9296);
         facilityRepository.save(facility1);
