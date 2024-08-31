0a052449d9b8cc614e5d76005ada90e0347b47a8
rivertw777 <105557972+rivertw777@users.noreply.github.com>
2024-08-31 16:21:23 +0900

    fix: api 경로 수정

--- api-server/src/main/java/DNA_Backend/api_server/global/security/config/SecurityConfig.java
+++ api-server/src/main/java/DNA_Backend/api_server/global/security/config/SecurityConfig.java
@@ -86,7 +86,7 @@ public class SecurityConfig {
                         // 시설 검색 by 위도, 경도 & 타입
                         .requestMatchers(antMatcher(HttpMethod.GET, "/api/facilities/search")).permitAll()
                         // 전체 워케이션 리뷰 조회
-                        .requestMatchers(antMatcher(HttpMethod.GET, "/api/reviews")).permitAll()
+                        .requestMatchers(antMatcher(HttpMethod.GET, "/api/reviews/all")).permitAll()
                         // Swagger
                         .requestMatchers("/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
